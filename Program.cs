6db139b276e7e4640bee39507f077e1aa7b95ee2
Reidar Nilsen <4reidar@gmail.com>
2023-09-21 10:58:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace NET22Lorentz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Manuellt test");
+            WeightCalculator calculator = new WeightCalculator
+            {
+                Height = 176,
+                Gender='m'
+            };
+            double result = calculator.GetIdealBodyWeight();
+            Console.WriteLine($"Kroppsvikten borde ligga på ca. {result}");
+
+            if( result == 77 ) 
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Testet lyckades");
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Testet misslyckades");
+            }
+            calculator.Gender = 'w';
+            double result2 = calculator.GetIdealBodyWeight();
+            if (result2 == 63)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Testet lyckades");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Testet misslyckades");
+            }
+        }
+        
+    }
+}
---
+++ WeightCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET22Lorentz
+{
+    internal class WeightCalculator
+    {
+        public double Height {  get; set; }
+        public char Gender {  get; set; }
+
+        public double GetIdealBodyWeight()
+        {
+            switch(Gender) 
+            {
+                case 'm':
+                    return (Height - 100) - ((Height - 150) / 4);
+                case 'w':
+                    return (Height - 100) - ((Height - 150) / 2);
+                default:
+                    return 0;
+            }
+        }
+    }
+}
